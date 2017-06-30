9917f3e54e925f66b786b4b34151b7e813f75f75
Sn8z <marcus.sneitz@telia.com>
2017-07-01 00:55:45 +0200

    added steamstatus command

--- mm-bot.rb
+++ mm-bot.rb
@@ -7,18 +7,36 @@ bot.message(with_text: '!help') do |event|
   event.respond 'Ring Poolia!'
 end
 
-bot.message(content: 'Ping!') do |event|
+bot.message(content: '!ping') do |event|
   m = event.respond('Pong!')
   m.edit "Pong! Time taken: #{Time.now - event.timestamp} seconds."
 end
 
 bot.message(content: '!roll') do |event|
-  event.respond("You rolled " + rand(1..100).to_s + "!")
+  event.respond("You rolled #{rand(1..100)}!")
 end
 
 bot.message(content: '!chuck') do |event|
   chuck = JSON.parse(open("https://api.chucknorris.io/jokes/random").read)
   event.respond(chuck["value"])
+end
+
+bot.message(content: '!steam') do |event|
+  steamJSON = JSON.parse(open("https://steamgaug.es/api/v2").read)
+  steam = "Steam status:\n"
+  steam += "Steam Client is #{getSteamStatus(steamJSON['ISteamClient']['online'])} \n"
+  steam += "Steam Community is #{getSteamStatus(steamJSON['SteamCommunity']['online'])} \n"
+  steam += "Steam Store is #{getSteamStatus(steamJSON['SteamStore']['online'])} \n"
+  steam += "Steam User is #{getSteamStatus(steamJSON['ISteamUser']['online'])}"
+  event.respond(steam)
+end
+
+def getSteamStatus(code)
+  if(code == 1)
+    return "online"
+  else
+    return "offline"
+  end
 end
 
 bot.run
