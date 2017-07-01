fa12e24301705a2101da4a684f191b136ac3dcdb
Sn8z <marcus.sneitz@telia.com>
2017-07-02 01:43:50 +0200

    changed to commands, rewrote roll, added wolfram

--- mm-bot.rb
+++ mm-bot.rb
@@ -1,36 +1,49 @@
 require 'discordrb'
+require 'wolfram'
 require 'open-uri'
 
-bot = Discordrb::Bot.new token: ENV["DISCORD_SECRET"], client_id: ENV["DISCORD_CLIENT_ID"]
+bot = Discordrb::Commands::CommandBot.new token: ENV["DISCORD_SECRET"], client_id: ENV["DISCORD_CLIENT_ID"], prefix: '!'
 
 bot.message(with_text: '!help') do |event|
   event.respond 'Ring Poolia!'
 end
 
-bot.message(content: '!ping') do |event|
+bot.command :ping do |event|
   m = event.respond('Pong!')
   m.edit "Pong! `Time taken: #{Time.now - event.timestamp} seconds.`"
 end
 
-bot.message(content: '!roll') do |event|
-  event.respond("#{event.author.username} rolled #{rand(1..100)}!")
+bot.command(:roll, min_args: 0, max_args: 2, usage: 'roll [min/max] [max]') do |event, min, max|
+  if max
+    number = rand(min.to_i..max.to_i)
+  elsif min
+    number = rand(1..min.to_i)
+  else
+    number = rand(1..100)
+  end
+  event.respond("#{event.author.username} rolled **#{number}**")
 end
 
-bot.message(content: '!chuck') do |event|
+bot.command :chuck do |event|
   chuck = JSON.parse(open("https://api.chucknorris.io/jokes/random").read)
   event.respond(chuck["value"])
 end
 
-bot.message(content: '!mom') do |event|
+bot.command :mom do |event|
   mom = JSON.parse(open("http://api.yomomma.info").read)
   event.respond(mom["joke"])
 end
 
-bot.message(content: '!source') do |event|
+bot.command :wolfram do |event, *args|
+  query = args.join('+')
+  event.respond(open("http://api.wolframalpha.com/v1/result?appid=#{ENV["WOLFRAM_SECRET"]}&i=#{query}").read)
+end
+
+bot.command :source do |event|
   event.respond("https://github.com/Sn8z/MM-bot")
 end
 
-bot.message(content: '!steam') do |event|
+bot.command :steam do |event|
   steamJSON = JSON.parse(open("https://steamgaug.es/api/v2").read)
   steam = "**Steam status:**\n"
   steam += "```Steam Client is #{getSteamStatus(steamJSON['ISteamClient']['online'])} \n"
